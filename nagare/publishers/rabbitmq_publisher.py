1d025cdccba5197908bcb64260f9d0fc8ce5c378
Alain Poirier <alain.poirier@net-ng.com>
2018-02-19 15:57:21 +0100

    Don't stop on exception

--- nagare/publishers/rabbitmq_publisher.py
+++ nagare/publishers/rabbitmq_publisher.py
@@ -11,13 +11,13 @@
 
 import time
 
-from nagare.server import publishers
+from nagare.server import publisher
 
 
-class Publisher(publishers.Publisher):
+class Publisher(publisher.Publisher):
     """The RabbitMQ publisher"""
 
-    CONFIG_SPEC = {'channel': 'string'}
+    CONFIG_SPEC = dict(publisher.Publisher.CONFIG_SPEC, channel='string')
 
     def __init__(self, name, dist, rabbitmq_service, services_service, **config):
         super(Publisher, self).__init__(name, dist, **config)
@@ -28,10 +28,16 @@ class Publisher(publishers.Publisher):
     def send(self, data):
         self.send_sock.send(data)
 
+    def start_handle_request(self, app, **params):
+        try:
+            return super(Publisher, self).start_handle_request(app, **params)
+        except Exception:
+            return None
+
     def _serve(self, app, channel):
         rabbitmq_channel = self.services[channel]
         rabbitmq_channel.on_receive(
-            lambda msg: app(channel=rabbitmq_channel, msg=msg)
+            lambda msg: self.start_handle_request(app, channel=rabbitmq_channel, msg=msg)
         )
 
         print time.strftime('%x %X -', time.localtime()), 'serving on channel', channel
