4a99142877ecd14b6c9cfbefbc94a02fe8e9b258
Alain Poirier <alain.poirier@net-ng.com>
2019-03-05 16:35:43 +0100

    Clean stop

--- nagare/publishers/rabbitmq_publisher.py
+++ nagare/publishers/rabbitmq_publisher.py
@@ -15,7 +15,10 @@ from nagare.server import publisher
 class Publisher(publisher.Publisher):
     """The RabbitMQ publisher"""
 
-    CONFIG_SPEC = dict(publisher.Publisher.CONFIG_SPEC, channel='string')
+    CONFIG_SPEC = dict(
+        publisher.Publisher.CONFIG_SPEC,
+        channel='string(help="name of the channel service to listen to")'
+    )
     has_multi_threads = True
 
     def __init__(self, name, dist, rabbitmq_service, services_service, **config):
@@ -28,9 +31,6 @@ class Publisher(publisher.Publisher):
         banner = super(Publisher, self).generate_banner()
         return banner + ' on channel `{}`'.format(self.plugin_config['channel'])
 
-    def send(self, data):
-        self.send_sock.send(data)
-
     def _serve(self, app, channel, **conf):
         super(Publisher, self)._serve(app)
 
@@ -39,6 +39,12 @@ class Publisher(publisher.Publisher):
             lambda msg: self.start_handle_request(app, channel=rabbitmq_channel, msg=msg)
         )
 
-        self.rabbitmq.start()
+        try:
+            rabbitmq_channel.start_consuming()
+        except KeyboardInterrupt:
+            rabbitmq_channel.stop_consuming()
+
+        rabbitmq_channel.close()
+        self.rabbitmq.close()
 
         return 0
